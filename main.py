f270c4634d7e8ab357953e83197f9fc5c43ab936
jag911 <tschmitzer@gmail.com>
2025-02-17 16:43:20 -0600

    BookBot L10 : count chars

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 def main():
     path_to_file = "books/frankenstein.txt"
     book_text = _get_file_contents(path_to_file)
-    words = _count_words(book_text)
-    print(words)
+    # words = _count_words(book_text)
+    char_dict = _count_letters(book_text)
+    print(char_dict)
 
 def _get_file_contents(path):
     with open(path) as f:
@@ -12,8 +13,15 @@ def _count_words(txt):
     tmp = txt.split()
     return len(tmp)
 
-
-
+def _count_letters(txt):
+    result = {}
+    for char in txt:
+        char = str(char).lower()
+        if not (char in result):
+            result[char] = 1
+        else:
+            result[char] += 1
+    return result
 
 main()
 exit()
