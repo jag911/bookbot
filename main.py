62c1bbd12ef587e226b87a95b69e9f200a946f9f
jag911 <tschmitzer@gmail.com>
2025-02-17 17:10:27 -0600

    BookBot L11 : Print a Report

--- main.py
+++ main.py
@@ -1,9 +1,23 @@
 def main():
-    path_to_file = "books/frankenstein.txt"
-    book_text = _get_file_contents(path_to_file)
-    # words = _count_words(book_text)
+    book_path = "books/frankenstein.txt"
+    # open and very simply read in the entire file
+    book_text = _get_file_contents(book_path)
+    #count how many "words" there are, consider any space as a seperator of a word
+    words = _count_words(book_text)
+    #get a dict of all characters used and how many times
+    #converted to lowercase, but keeping non-alpha chars
     char_dict = _count_letters(book_text)
-    print(char_dict)
+    alpha = _only_letter_count(char_dict)
+    alpha_sorted = dict(sorted(alpha.items(), key=lambda item: item[1], reverse=True))  #PLAYGROUND
+    _print_report(book_path,words,alpha_sorted)
+
+
+def _print_report(path,word_count,char_count):
+    print(f"--- Begin report of {path} ---")
+    print(f"{word_count} words found in the document\n")
+    for char in char_count:
+        print(f"The '{char}' character was found {char_count[char]} times")
+    print("--- End report ---")
 
 def _get_file_contents(path):
     with open(path) as f:
@@ -22,6 +36,15 @@ def _count_letters(txt):
         else:
             result[char] += 1
     return result
+
+def _only_letter_count(dict_in):
+    alphabet = "abcdefghijklmnopqrstuvwxyz"
+    result = {}
+    for entry in dict_in:
+        if entry in alphabet:
+            result[entry] = dict_in[entry]
+    return result
+            
 
 main()
 exit()
